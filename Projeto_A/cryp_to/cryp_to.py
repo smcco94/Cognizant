0108f9c108651e84ac179c5862c3be8285d26232
smcco94 <sergiomcastro94@gmail.com>
2022-04-14 22:31:24 -0300

    Atualização classe

--- Projeto_A/cryp_to/cryp_to.py
+++ Projeto_A/cryp_to/cryp_to.py
@@ -1,16 +1,18 @@
-def cryp_to(passwd):
-    new_passwd = []
-    passwd_to = []
-    for i in passwd:
-        passwd_to.append((ord(i)))
-    new_passwd = ';'.join(map(str, passwd_to))
-    print("Sua senha é: {}".format(new_passwd))
+class crypto:
+    
+    def cryp_to(passwd):
+        new_passwd = []
+        passwd_to = []
+        for i in passwd:
+            passwd_to.append((ord(i)))
+        new_passwd = ';'.join(map(str, passwd_to))
+        print("Sua senha é: {}".format(new_passwd))
 
-def cryp_turn(passwd):
-    turn_passwd = []
-    original = []
-    turn_passwd.extend(passwd.split((';')))
-    for i in turn_passwd:
-        original.append(chr(int(i)))
-    old_passwd = ''.join(map(str, original))
-    print("Sua senha original é: {}".format(old_passwd))
+    def cryp_turn(passwd):
+        turn_passwd = []
+        original = []
+        turn_passwd.extend(passwd.split((';')))
+        for i in turn_passwd:
+            original.append(chr(int(i)))
+        old_passwd = ''.join(map(str, original))
+        print("Sua senha original é: {}".format(old_passwd))
